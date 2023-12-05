8a0415bf723ebd5760788b1a28b0f14d7bd9206e
Drew Wysocki <wysockid10@gmail.com>
2023-12-04 19:50:25 -0800

    egrep for + and - symbols

--- test.sh
+++ test.sh
@@ -7,3 +7,13 @@ egrep 'commit.*tag:' git-log.txt
 echo $?
 egrep -v 'commit.*tag:' git-log.txt
 echo $?
+
+egrep 'comit' git-log-should-match.txt
+echo $?
+#brings up a 1 for nothing matching
+egrep -v 'comit' git-log-should-match.txt
+echo $?
+egrep 'commit.*tag:' git-log-should-not-match.txt
+echo $? 
+egrep -v 'commit.*tag:' git-log-should-not-match.txt
+echo $?
