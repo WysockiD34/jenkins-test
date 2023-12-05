9287ce6cc943c6b3987148d5ab4bddd3309924e8
Kit Transue <noreply>
2023-12-04 17:43:12 -0800

    exposing error codes

--- test.sh
+++ test.sh
@@ -2,3 +2,8 @@
 
 # this works pretty well:
 egrep 'commit.*tag' git-log.txt
+echo $?
+egrep 'commit.*tag' git-log-should-match.txt
+echo $?
+egrep -v 'commit.*tag' git-log-should-match.txt
+echo $?
