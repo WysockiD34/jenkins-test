a3c5c2b8cfef5e1aefaa4e6a3ca5f4092eb58d35
Drew Wysocki <wysockid10@gmail.com>
2023-12-07 15:19:42 -0800

    more changes to finding-auth.sh

--- finding-auth.sh
+++ finding-auth.sh
@@ -6,3 +6,15 @@ echo $?
 #this will bring up the text that is contained inside of auth-should-not-match
 egrep -v 'New\ssession' auth.log
 echo $?
+
+PATTERN='New\ssession'
+
+failure=0
+if egrep -v "$PATTERN" auth-should-match.log
+then
+failure=1
+echo "failed! leak in auth"
+else
+echo "success on auth"
+fi
+echo $?
