5393b6b1ecd724727f1683a395e25f7875a9b8a7
Drew Wysocki <wysockid10@gmail.com>
2023-12-06 18:18:27 -0800

    working on plus and minus

--- plus-minus-test.sh
+++ plus-minus-test.sh
@@ -1,1 +1,26 @@
 #!/usr/bin/env bash
+egrep '^\+\s.*\^d' plus-should-match.txt
+echo $?
+
+PATTERN='^\+\s.*\^d'
+
+failure=0
+if egrep -v "$PATTERN" plus-should-match.txt
+then
+failure=1
+echo "failed! leak in plus-minus"
+else
+echo "success on plus-minus"
+fi
+echo $?
+
+failure=0
+if egrep "$PATTERN" plus-should-not-match.txt
+then
+failure=1
+echo "failed! leak in plus-minus"
+else
+echo "success on plus-minus"
+fi
+echo $?
+
